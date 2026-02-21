3d381f0ff753a06514eff780886dea5fc6a01720
Narethputponleu Hong <hongnarethputponleu015@gmail.com>
2026-02-22 00:39:57 +0700

    Setting up Vite and change jsx to tsx

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Feature Coming Up</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/web/src/vite-env.d.ts
@@ -0,0 +1,9 @@
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
